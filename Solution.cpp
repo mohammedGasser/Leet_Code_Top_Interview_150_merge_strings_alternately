29a883d3db10a53f55204a3e6875aebb6313e107
mohammedGasser <mohamedgassermohamede@gmail.com>
2025-03-25 09:15:52 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+       string mergeAlternatel;
+       int i_1=word1.length();
+       int i_2=word2.length();
 
+       word1.append(0,'\0');
+         word2.append(0,'\0');
+      int m_=i_1+i_2;
+   //   mergeAlternatel.append(word1);
+        
+ // mergeAlternatel+=word1[0];
+          for(int i=0; i<i_1+i_2;i++ )
+                { 
+                    if(i<word1.size()) 
+                     mergeAlternatel+=word1[i];
+                 
+                    if(i<word2.size())
+              { mergeAlternatel+=word2[i];}
+               
+               
+       }
+        return mergeAlternatel; }
+};
